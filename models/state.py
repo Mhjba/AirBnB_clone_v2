967a45139e1ee7158ea1430f4c48e2c207d5fdb2
Mhjba <mahjoubaaitalla15@gmail.com>
2024-03-17 12:44:27 +0000

    airbnb v2

---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" State Module for HBNB project """
+from models.base_model import BaseModel
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String
+Base = declarative_base()
+
+
+class State(BaseModel, Base):
+    """ State class """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
