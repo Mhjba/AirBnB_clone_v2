798a89bcca1f24c00fb25527081142fa99a48c23
Mhjba <mahjoubaaitalla15@gmail.com>
2024-03-17 15:31:00 +0000

    airbnb

--- models/city.py
+++ models/city.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
-from models.base_model import BaseModel
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, ForeignKeyConstraint
-Base = declarative_base()
+''' City Module for HBNB project '''
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
+    ''' The City Class '''
     __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), nullable=False)
 
-    __table_args__ = (
-        ForeignKeyConstraint(['state_id'], ['state.id']),
-    )
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60),
+                          ForeignKey('states.id', ondelete='CASCADE'),
+                          nullable=False)
+        places = relationship("Place", backref="cities", cascade="all, delete")
+    else:
+        name = ''
+        state_id = ''
