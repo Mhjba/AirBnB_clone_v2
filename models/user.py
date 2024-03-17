457127fd729ba638418d32fc70ae4b0bd7586aed
Mhjba <mahjoubaaitalla15@gmail.com>
2024-03-17 15:38:49 +0000

    airbnb v2

--- models/user.py
+++ models/user.py
@@ -7,19 +7,8 @@ from os import getenv
 
 
 class User(BaseModel, Base):
-    ''' The User class '''
-
     __tablename__ = 'users'
-
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user", cascade="all, delete")
-        reviews = relationship("Review", backref="user", cascade="all, delete")
-    else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
