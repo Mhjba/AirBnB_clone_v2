bd837652152b6787d8a1abbc5541260dd539de95
Mhjba <mahjoubaaitalla15@gmail.com>
2024-03-18 12:14:41 +0000

    v2

--- models/state.py
+++ models/state.py
@@ -1,32 +1,30 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-import os
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-
+from models import storage
 from models.base_model import BaseModel, Base
 from models.city import City
+import os
 
 
 class State(BaseModel, Base):
-    """ State class """
+    """ The State class """
     __tablename__ = 'states'
-    name = Column(
-        String(128), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+
+    name = Column(String(128), nullable=False)
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship(
-            'City',
-            cascade='all, delete, delete-orphan',
-            backref='state'
-        )
+
+        cities = relationship('City', cascade='all, delete, delete-orphan', backref='state')
     else:
+        name = ''
+
         @property
         def cities(self):
             """Returns the cities in this State"""
-            from models import storage
-            cities_in_state = []
+            cities_state = []
             for value in storage.all(City).values():
                 if value.state_id == self.id:
-                    cities_in_state.append(value)
-            return cities_in_state
+                    cities_state.append(value)
+            return cities_state
+
--- models/user.py
+++ models/user.py
@@ -3,15 +3,14 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from os import getenv
+import os
 
 
 class User(BaseModel, Base):
     ''' The User class '''
 
     __tablename__ = 'users'
-
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
