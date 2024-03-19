4c3afe8cb4c0280ec93eec3fd8d18a9ab50a4772
Mhjba <mahjoubaaitalla15@gmail.com>
2024-03-19 17:15:13 +0000

    v2

--- models/amenity.py
+++ models/amenity.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel, Base
+""" Amenity Module for HBNB project """
+import os
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.place import place_amenity
+from models.base_model import BaseModel, Base
 
 
 class Amenity(BaseModel, Base):
-    """Amenity class that inherits from BaseModel and Base"""
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity)
+    """The Amenity Class"""
+    __tablename__ = 'amenities'
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+    
+    else:
+        name = ""
