e7b6f99758b073f87401ec1ff2cbbc0bd21eb3d9
Mhjba <mahjoubaaitalla15@gmail.com>
2024-03-20 11:03:08 +0000

    v2

--- models/amenity.py
+++ models/amenity.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
-"""Defines the Amenity class."""
-from models.base_model import Base
-from models.base_model import BaseModel
-from sqlalchemy import Column
-from sqlalchemy import String
-from sqlalchemy.orm import relationship
+""" Amenity Module for HBNB project """
+import os
+from sqlalchemy import Column, String
+from models.base_model import BaseModel, Base
 
 
 class Amenity(BaseModel, Base):
-    """Represents an Amenity for a MySQL database.
-    Inherits from SQLAlchemy Base and links to the MySQL table amenities.
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store Amenities.
-        name (sqlalchemy String): The amenity name.
-        place_amenities (sqlalchemy relationship): Place-Amenity relationship.
-    """
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity",
-                                   viewonly=False)
+    """The Amenity Class"""
+    __tablename__ = 'amenities'
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
