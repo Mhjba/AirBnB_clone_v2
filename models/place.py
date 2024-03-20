50a7ae3a5913c5c84d927b516512bb1745184b93
Mhjba <mahjoubaaitalla15@gmail.com>
2024-03-20 11:46:42 +0000

    v2

--- models/place.py
+++ models/place.py
@@ -6,15 +6,12 @@ from sqlalchemy.orm import relationship
 from os import getenv
 from models.amenity import Amenity
 
-place_amenity = Table('place_amenity', Base.metadata,
-                      Column('place_id', String(60),
+place_amenity = Table('place_amenity', Base.metadata, Column('place_id', String(60),
                              ForeignKey('places.id'),
-                             primary_key=True, nullable=False),
-                      Column('amenity_id', String(60),
+                             primary_key=True, nullable=False), Column('amenity_id', String(60),
                              ForeignKey('amenities.id'),
                              primary_key=True, nullable=False),
-                      )
-
+                             )
 
 class Place(BaseModel, Base):
     """ A place to stay """
