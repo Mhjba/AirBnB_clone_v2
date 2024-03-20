cd66024e98f9389e33d2e3399f4341194a2d9c69
Mhjba <mahjoubaaitalla15@gmail.com>
2024-03-20 12:05:46 +0000

    v2

---
+++ models/amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Amenity Module for HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+
+
+class Amenity(BaseModel, Base):
+    """ The Amenity Class """
+    __tablename__ = 'amenities'
+
+    name = Column(String(128), nullable=False)
