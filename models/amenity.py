63652f7e2a1d2bf07e91429cfc19793765ac3013
Mhjba <mahjoubaaitalla15@gmail.com>
2024-03-20 12:08:58 +0000

    v2

--- models/amenity.py
+++ models/amenity.py
@@ -2,10 +2,13 @@
 """ Amenity Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-
+import so
 
 class Amenity(BaseModel, Base):
     """ The Amenity Class """
     __tablename__ = 'amenities'
+    if so.getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
 
-    name = Column(String(128), nullable=False)
+    else:
+        name = ""
