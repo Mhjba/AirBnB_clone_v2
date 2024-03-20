c80bef85b7b65c13f4005d096a776de84dadde85
Mhjba <mahjoubaaitalla15@gmail.com>
2024-03-20 11:08:09 +0000

    v2

--- models/amenity.py
+++ models/amenity.py
@@ -2,14 +2,15 @@
 """ Amenity Module for HBNB project """
 import os
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 
 
 class Amenity(BaseModel, Base):
-    """The Amenity Class"""
+    """Represents an amenity data set."""
     __tablename__ = 'amenities'
-
+    
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
     else:
-        name = ""
+        name = None
