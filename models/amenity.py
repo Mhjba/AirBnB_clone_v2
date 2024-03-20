0122cce634533edc4931592d8a708735812eee86
Mhjba <mahjoubaaitalla15@gmail.com>
2024-03-20 11:11:12 +0000

    v2

--- models/amenity.py
+++ models/amenity.py
@@ -3,14 +3,13 @@
 import os
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+
 from models.base_model import BaseModel, Base
 
 
 class Amenity(BaseModel, Base):
     """Represents an amenity data set."""
     __tablename__ = 'amenities'
-    
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-    else:
-        name = None
+    name = Column(
+        String(128), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
