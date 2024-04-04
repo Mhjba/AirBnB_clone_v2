305bb63bbbe2d65f346948e2595dc1e4080710c5
Mhjba <mahjoubaaitalla15@gmail.com>
2024-04-04 10:22:45 -0700

    v2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-'''
-    Deploy web_static on Servers
-'''
-from fabric.api import run, put, env, local
-from os.path import exists
-from datetime import datetime
+"""
+Distributes an archive to my web servers,
+using the function do_deploy
+"""
 from os.path import isdir
+from datetime import datetime
+import os
+from fabric.api import env, local, put, run
 
-env.hosts = ["52.55.249.213", "	54.157.32.137"]
+
+env.hosts = ['54.157.32.137', '52.55.249.213']
 
 
 def do_pack():
@@ -30,21 +32,23 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    ''' deploy archive to servers '''
-    if exists(archive_path) is False:
+    '''
+    Deploy archive to web server
+    '''
+    if not os.path.exists(archive_path):
         return False
+    file_name = archive_path.split('/')[1]
+    file_path = '/data/web_static/releases/'
+    releases_path = file_path + file_name[:-4]
     try:
-        file_path = archive_path.split("/")[-1]
-        file_name = file_path.split(".")[0]
-        des_path = "/data/web_static/releases/"
-        put(archive_path, f"/tmp/")
-        run(f"mkdir -p {des_path}/{file_name}")
-        run(f"tar -xzf /tmp/{file_path} -C {des_path}/{file_name}/")
-        run(f"rm /tmp/{file_path}")
-        run(f'mv {des_path}/{file_name}/web_static/* {des_path}/{file_name}/')
-        run(f'rm -rf {des_path}/{file_name}/web_static')
-        run(f"rm -rf /data/web_static/current")
-        run(f"ln -s {des_path}/{file_name}/ /data/web_static/current")
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}'.format(releases_path))
+        run('tar -xzf /tmp/{} -C {}'.format(file_name, releases_path))
+        run('rm /tmp/{}'.format(file_name))
+        run('mv {}/web_static/* {}/'.format(releases_path, releases_path))
+        run('rm -rf {}/web_static'.format(releases_path))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(releases_path))
         return True
     except Exception:
         return False
