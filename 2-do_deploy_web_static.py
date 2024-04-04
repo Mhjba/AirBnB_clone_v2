5c7c34dd627d8518ab9f88f4d6e357ca337c0948
Mhjba <mahjoubaaitalla15@gmail.com>
2024-04-04 09:02:45 -0700

    v2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """A module for web application deployment with Fabric."""
 import os
+from os.path import isdir
 from datetime import datetime
 from fabric.api import env, local, put, run, runs_once
 
@@ -11,26 +12,22 @@ env.hosts = ["34.73.0.174", "35.196.78.105"]
 
 @runs_once
 def do_pack():
-    """Archives the static files."""
-    if not os.path.isdir("versions"):
-        os.mkdir("versions")
-    cur_time = datetime.now()
-    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        cur_time.year,
-        cur_time.month,
-        cur_time.day,
-        cur_time.hour,
-        cur_time.minute,
-        cur_time.second
-    )
-    try:
-        print("Packing web_static to {}".format(output))
-        local("tar -cvzf {} web_static".format(output))
-        archize_size = os.stat(output).st_size
-        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
-    except Exception:
-        output = None
-    return output
+    """Archives the static files_name"""
+    dat_t = datetime.now()
+    file_name = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        dat_t.year,
+        dat_t.month,
+        dat_t.day,
+        dat_t.hour,
+        dat_t.minute,
+        dat_t.second)
+
+    if isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file_name)).failed is True:
+        return None
+    return
 
 
 def do_deploy(archive_path):
