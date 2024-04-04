a4c41fab29a2777e72fd04c5d22547bd57026e1e
Mhjba <mahjoubaaitalla15@gmail.com>
2024-04-04 08:33:45 -0700

    v2

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -11,7 +11,13 @@ sudo mkdir -p '/data/web_static/releases/test/'
 sudo mkdir -p '/data/web_static/shared/'
 
 # Create a fake HTML file for testing
-echo "<html><head></head><body>Holberton School</body></html>" | sudo tee '/data/web_static/releases/test/index.html' > /dev/null
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee '/data/web_static/releases/test/index.html' > /dev/null
 # Create symbolic link
 sudo ln -sf '/data/web_static/releases/test/' '/data/web_static/current'
 # Set ownership recursively
