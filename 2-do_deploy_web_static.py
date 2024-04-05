c9e56e1835caef3f36e7c080c3b5c11df67232b4
Mhjba <mahjoubaaitalla15@gmail.com>
2024-04-05 04:05:37 -0700

    v2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-"""
-Distributes an archive to my web servers,
-using the function do_deploy
-"""
-from os.path import isdir
+'''
+    Deploy web_static on Servers
+'''
+from fabric.api import run, put, env, local
+from os.path import exists
 from datetime import datetime
-import os
-from fabric.api import env, local, put, run
+from os.path import isdir
 
-
-env.hosts = ['54.172.176.21', '54.234.38.223']
+env.hosts = ["54.172.176.21", "	54.234.38.223"]
 
 
 def do_pack():
@@ -32,23 +30,21 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    '''
-    Deploy archive to web server
-    '''
-    if not os.path.exists(archive_path):
+    ''' deploy archive to servers '''
+    if exists(archive_path) is False:
         return False
-    file_name = archive_path.split('/')[1]
-    file_path = '/data/web_static/releases/'
-    releases_path = file_path + file_name[:-4]
     try:
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}'.format(releases_path))
-        run('tar -xzf /tmp/{} -C {}'.format(file_name, releases_path))
-        run('rm /tmp/{}'.format(file_name))
-        run('mv {}/web_static/* {}/'.format(releases_path, releases_path))
-        run('rm -rf {}/web_static'.format(releases_path))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {} /data/web_static/current'.format(releases_path))
+        file_path = archive_path.split("/")[-1]
+        file_name = file_path.split(".")[0]
+        des_path = "/data/web_static/releases/"
+        put(archive_path, f"/tmp/")
+        run(f"mkdir -p {des_path}/{file_name}")
+        run(f"tar -xzf /tmp/{file_path} -C {des_path}/{file_name}/")
+        run(f"rm /tmp/{file_path}")
+        run(f'mv {des_path}/{file_name}/web_static/* {des_path}/{file_name}/')
+        run(f'rm -rf {des_path}/{file_name}/web_static')
+        run(f"rm -rf /data/web_static/current")
+        run(f"ln -s {des_path}/{file_name}/ /data/web_static/current")
         return True
     except Exception:
         return False
