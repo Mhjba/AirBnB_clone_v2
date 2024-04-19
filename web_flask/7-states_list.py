fe6f27c4e65409113b89d3b46c5df446f1e8921b
Mhjba <mahjoubaaitalla15@gmail.com>
2024-04-19 20:17:22 +0000

    web flask

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
-'''A flask app displaying states in a web page'''
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 
 app = Flask(__name__)
 
-
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    ''' return all states '''
-    states = sorted(list(storage.all(State).values()), key=lambda x: x.name)
-    return(render_template('7-states_list.html', states=states))
+    """Display a list of all State objects sorted by name."""
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda state: state.name)
 
+    return render_template('states_list.html', states=sorted_states)
 
 @app.teardown_appcontext
-def teardown_session(exception):
-    """closes the storage on teardown"""
+def close_storage(exception):
+    """Remove the current SQLAlchemy Session after each request."""
     storage.close()
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
