455dbc7ead7fe362b74e293d9604a987993fdf14
Mhjba <mahjoubaaitalla15@gmail.com>
2024-04-19 19:55:10 +0000

    web flask

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Start flask app for HBNB """
+	"""Start flask app for HBNB """
 from models import storage
 from flask import Flask
 from flask import render_template
@@ -9,16 +9,16 @@ app = Flask(__name__)
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
-    """ Displays an HTML page """
+    """ displays an HTML page """
     states = storage.all("State")
     return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
 def teardown(app):
-    """Declare a method to handle """
+    """ declare a method to handle """
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
