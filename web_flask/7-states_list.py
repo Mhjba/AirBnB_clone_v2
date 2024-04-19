9b69782e573b5be2fbebabbb3ad9b4bbe2b34ad3
Mhjba <mahjoubaaitalla15@gmail.com>
2024-04-19 20:01:03 +0000

    web flask

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,6 +5,7 @@ from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.route("/states_list", strict_slashes=False)
@@ -20,5 +21,5 @@ def teardown(app):
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
