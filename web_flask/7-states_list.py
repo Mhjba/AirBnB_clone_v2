c323943b661ed5357b4cf40776900559e9bfd0e4
Mhjba <mahjoubaaitalla15@gmail.com>
2024-04-20 10:58:51 +0000

    web flask

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
-""" Starts a Flask web app """
+"""This module defines a class to manage file storage for hbnb clone"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def dis_pose(exception):
-    """ Remove current session """
+def teardown_db(_):
+    """Close storage."""
     storage.close()
 
 
-@app.route('/states_list')
-def state_s():
-    """ Display list of all the states """
-    state_s = storage.all(State)
-    state_s_list = list(state_s.values())
-    return render_template('7-states_list.html', state_s=states_list)
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Display a HTML page."""
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
 
 
 if __name__ == '__main__':
