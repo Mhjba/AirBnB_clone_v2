2d4830e880c4db5a508759db4c115e147701a526
Mhjba <mahjoubaaitalla15@gmail.com>
2024-05-13 17:53:17 +0000

    clone v2

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """ start a flask web app """
 
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def hello_hbnb():
     """ display "Hello HBNB!" """
-    return 'Hello HBNB!'
+    return render_template("10-hbnb_filters.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
